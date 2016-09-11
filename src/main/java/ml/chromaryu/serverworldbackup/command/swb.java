661f4a8302d2ca5f1d4c4471bb29d243e110286f
knight-ryu12 <knight.ryu12@gmail.com>
2016-09-11 17:25:16 +0900

    Implimented :P

--- src/main/java/ml/chromaryu/serverworldbackup/command/swb.java
+++ src/main/java/ml/chromaryu/serverworldbackup/command/swb.java
@@ -1,5 +1,7 @@
 package ml.chromaryu.serverworldbackup.command;
 
+import ml.chromaryu.serverworldbackup.api.compressTarGz;
+import ml.chromaryu.serverworldbackup.main;
 import org.bukkit.Bukkit;
 import org.bukkit.World;
 import org.bukkit.command.Command;
@@ -8,8 +10,16 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
+import java.util.Locale;
+import java.util.logging.Logger;
+
 
 /**
  * Created by ryuniverse on 2016/09/06.
@@ -19,15 +29,44 @@ public class swb implements CommandExecutor {
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] args) {
         if(commandSender instanceof Player) {
             Player p = (Player) commandSender;
-            if(args[0].equals("saveworld")) {
-                World w = p.getWorld();
-                File worldFile = w.getWorldFolder();
-                File[] files = worldFile.listFiles();
-                for (File f: files) {
-                    Bukkit.getLogger().info(f.getPath());
+            Logger logger = Bukkit.getLogger();
+            if(args.length >= 1) {
+                if (args[0].equalsIgnoreCase("saveworld")) {
+                    long initTime = System.currentTimeMillis();
+                    p.sendMessage(main.prefix + "Saving the current world!");
+                    String filename = main.dir + "/" + p.getWorld().getName() + " " +new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ", Locale.US).format(Calendar.getInstance().getTime()) + ".tar.gz";
+                    List<Path> fileList = new ArrayList<>();
+                    FileVisitor<Path> visitor = new SimpleFileVisitor<Path>() {
+                        @Override
+                        public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                            fileList.add(file);
+                            return FileVisitResult.CONTINUE;
+                        }
+                    };
+                    try {
+                        Files.walkFileTree(new File(p.getWorld().getWorldFolder().getPath().substring(2)).toPath(), visitor);
+                        compressTarGz.compresstargz(fileList, filename);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    initTime = System.currentTimeMillis() - initTime;
+                    p.sendMessage(main.prefix + "Done in " + initTime + "ms");
+                    return true;
                 }
+                if (args[0].equalsIgnoreCase("saveallworld")) {
+                    // TODO: 16/09/11 Impliment "get all World"
+                    return true;
+                }
+                if (args[0].equalsIgnoreCase("test")) {
+                    p.sendMessage(p.spigot().getLocale());
+                }
+            } else {
+                p.sendMessage(main.prefix + "");
                 return true;
             }
+        } else {
+            Bukkit.getLogger().info("Use this command in-game!");
+            return false;
         }
         return false;
     }
--- src/main/java/ml/chromaryu/serverworldbackup/main.java
+++ src/main/java/ml/chromaryu/serverworldbackup/main.java
@@ -1,9 +1,11 @@
 package ml.chromaryu.serverworldbackup;
 
 import ml.chromaryu.serverworldbackup.command.swb;
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
+import java.util.logging.Logger;
 
 /**
  * Created by ryuniverse on 2016/09/06.
@@ -11,10 +13,17 @@ import java.io.File;
 public class main extends JavaPlugin {
     long initTime;
     File config;
+    public static String prefix;
+    public static Logger logger;
+    public static File dir;
     public void onEnable() {
         initTime = System.currentTimeMillis(); // get Time in millis to determine how much time did this plugin take to initialize
         getCommand("swb").setExecutor(new swb());
-        saveDefaultConfig();
+        logger = getLogger();
+        dir = getDataFolder();
+        getLogger().info(dir.toString() + " " + dir.toPath());
+        createConfig();
+        prefix = ChatColor.translateAlternateColorCodes('&',getConfig().getString("Pluginprefix"));
         config = new File(getDataFolder(),"config.yml");
         initTime = initTime - System.currentTimeMillis();
         getLogger().info("Initialization was completed in" + initTime +"ms.");
@@ -26,4 +35,19 @@ public class main extends JavaPlugin {
         getLogger().info("Plugin successfully unloaded!");
     }
 
+    private void createConfig() {
+        try {
+            File file = new File(getDataFolder(), "config.yml");
+            if (!file.exists()) {
+                getLogger().info("Config.yml not found, creating!");
+                saveDefaultConfig();
+            } else {
+                getLogger().info("Config.yml found, loading!");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+
+        }
+
+    }
 }
